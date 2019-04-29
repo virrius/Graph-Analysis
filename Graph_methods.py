405691437944c9d2f84b253d44676d23774eee22
virrius <virrius@ro.ru>
2019-04-29 04:23:34 +0300

    bad

--- Graph_methods.py
+++ Graph_methods.py
@@ -22,7 +22,7 @@ def dfs(gr, start):
     return visited
 
 
-def paths_bfs(gr, start):
+def paths_bfs(gr, start, v=None):
     visited = set()
     paths = defaultdict(lambda: 0)
     queue = deque()
@@ -43,6 +43,41 @@ def paths_bfs(gr, start):
     return paths
 
 
+def count_shortest_paths_with_keypoint(gr, start, keypoint):
+    visited = set()
+    queue = deque()
+    queue.append(start)
+    layer_queue = deque()
+    paths_count = 1
+    keypoint_visited = set()
+    keypoint_visited.add(keypoint)
+    tmp = False
+    c=0
+
+    while queue or layer_queue:
+
+        if not queue:
+            queue.extend(list(layer_queue))
+            if(keypoint in queue):
+                queue.appendleft(keypoint)
+            layer_queue.clear()
+
+        vertex = queue.popleft()
+        if vertex not in visited:
+            visited.add(vertex)
+            if vertex in keypoint_visited:
+                paths_count += 1
+                keypoint_visited.update(set(neighbors(gr, vertex)))
+                keypoint_visited= keypoint_visited - visited
+                queue.extendleft(keypoint_visited)
+            else:
+                layer_queue.extend(set(neighbors(gr, vertex)) - visited)
+
+
+    print(c)
+    return paths_count
+
+
 def ordered_dfs(gr, start, order, marked):
 
     marked.add(start)
@@ -52,8 +87,8 @@ def ordered_dfs(gr, start, order, marked):
     order.append(start)
 
 
-def graph_metrics(graph):
-    file = open('text.txt', 'w')
+def graph_coefficients(graph):
+    file = open('coefficients.txt', 'w')
     file.write("---------------Common neighbours--------------- \n\n")
     for v1 in graph.nodes():
         for v2 in graph.nodes():
@@ -72,11 +107,38 @@ def graph_metrics(graph):
             if v1 != v2:
                 file.write(v1 + ' ' + v2 + ' -> ' + str(graph.nodes_adamic_adar(v1, v2)) + '\n')
 
-    file.write("\n\n---------------nodes_preferential_attachment--------------- \n\n")
+    file.write("\n\n---------------preferential_attachment--------------- \n\n")
     for v1 in graph.nodes():
         for v2 in graph.nodes():
             if v1 != v2:
-                file.write(v1 + ' ' + v2 + ' -> ' + str(graph.nodes_adamic_adar(v1, v2)) + '\n')
+                file.write(v1 + ' ' + v2 + ' -> ' + str(graph.nodes_preferential_attachment(v1, v2)) + '\n')
+
+
+def graph_metrics(graph):
+    file = open('metrics.txt', 'w')
+    file.write("---------------degree centrality--------------- \n\n")
+    for v in graph.nodes():
+        file.write(v + ' -> ' + str(graph.degree(v)/len(graph.nodes())) + '\n')
+
+    file.write("\n\n---------------closeness centrality--------------- \n\n")
+    for v in graph.nodes():
+        sum = 0
+        for v1 in graph.nodes():
+            sum += graph.Paths[v][v1]
+        file.write(v + ' -> ' + str(len(graph.nodes())/sum) + '\n')
+
+    file.write("\n\n---------------closeness centrality--------------- \n\n")
+
+    for key_v in graph.nodes():
+        sum = 0
+        for v1 in graph.nodes():
+
+            if v1 != key_v:
+
+                sum += count_shortest_paths_with_keypoint(graph.Graph,v1,key_v)
+        file.write(key_v + ' -> ' + str(sum/len(graph.nodes())) + '\n')
+
+
 
 
 class GraphClass:
